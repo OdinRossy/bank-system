2ad603a74cbd9f503767cc071ddd5a82c484cf7a
Gleb Remniov <remniov.work@gmail.com>
2019-04-13 17:02:44 +0300

    Update AuthorizationService

--- src/main/java/com/odinrossy/banksystem/services/security/AuthorizationService.groovy
+++ src/main/java/com/odinrossy/banksystem/services/security/AuthorizationService.groovy
@@ -1,9 +1,9 @@
 package com.odinrossy.banksystem.services.security
 
 import com.odinrossy.banksystem.models.worker.Worker
-import com.odinrossy.banksystem.services.ApplicationService
+import com.odinrossy.banksystem.services.logger.LoggerService
 
-interface AuthorizationService extends ApplicationService {
+interface AuthorizationService extends LoggerService {
 
     Worker getWorkerFromSession()
 
