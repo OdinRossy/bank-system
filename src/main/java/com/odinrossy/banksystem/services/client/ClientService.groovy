39fc4fe2de0ff63b8faa254244c13d4cec1ccee0
Gleb Remniov <remniov.work@gmail.com>
2019-04-14 17:45:02 +0300

    Create Client service

---
+++ src/main/java/com/odinrossy/banksystem/services/client/ClientService.groovy
@@ -0,0 +1,25 @@
+package com.odinrossy.banksystem.services.client
+
+import com.odinrossy.banksystem.exceptions.ResourceAlreadyExistsException
+import com.odinrossy.banksystem.exceptions.ResourceNotFoundException
+import com.odinrossy.banksystem.exceptions.ResourceNotValidException
+import com.odinrossy.banksystem.models.client.Client
+import com.odinrossy.banksystem.services.logger.LoggerService
+import org.springframework.stereotype.Service
+
+@Service
+interface ClientService extends LoggerService {
+
+    List<Client> findAll()
+
+    Client findById(long id) throws ResourceNotFoundException
+
+    Client findByPassportId(id) throws ResourceNotFoundException
+
+    Client save(Client client) throws ResourceAlreadyExistsException, ResourceNotValidException
+
+    Client update(long id, Client client) throws ResourceNotFoundException, ResourceNotValidException
+
+    def delete(long id) throws ResourceNotFoundException
+
+}
---
+++ src/main/java/com/odinrossy/banksystem/services/client/ClientServiceImpl.groovy
@@ -0,0 +1,79 @@
+package com.odinrossy.banksystem.services.client
+
+import com.odinrossy.banksystem.exceptions.ResourceAlreadyExistsException
+import com.odinrossy.banksystem.exceptions.ResourceNotFoundException
+import com.odinrossy.banksystem.exceptions.ResourceNotValidException
+import com.odinrossy.banksystem.models.client.Client
+import com.odinrossy.banksystem.repositories.client.ClientRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class ClientServiceImpl implements ClientService {
+
+    @Autowired
+    ClientRepository clientRepository
+
+    @Override
+    List<Client> findAll() {
+        return clientRepository.findAll() as List<Client>
+    }
+
+    @Override
+    Client findById(long id) throws ResourceNotFoundException {
+        log.debug("findById, id: ${id}.")
+
+        Client registrationFromDB = clientRepository.findById(id)
+
+        if (!registrationFromDB)
+            throw new ResourceNotFoundException("Client not found. Id: ${id}")
+
+        return registrationFromDB
+    }
+
+    @Override
+    Client findByPassportId(Object id) throws ResourceNotFoundException {
+        return null
+    }
+
+    @Override
+    Client save(Client client) throws ResourceNotValidException {
+        log.debug("save, ${client}.")
+
+        try {
+            def clientFromDB = findById(client.id)
+            throw new ResourceAlreadyExistsException("Client already exists. ${clientFromDB}")
+
+        } catch (ResourceNotFoundException ignored) {
+            if (!client) {
+                throw new ResourceNotValidException("Client not valid. ${client}")
+            }
+
+//            todo return findById(client.id) will better
+            return clientRepository.save(client)
+        }
+    }
+
+    @Override
+    Client update(long id, Client client) throws ResourceNotFoundException, ResourceNotValidException {
+        client.id = id
+        log.debug("update, ${client}.")
+
+        findById(client.id)
+
+        if (!client) {
+            throw new ResourceNotValidException("Client not valid. ${client}")
+        }
+
+        return clientRepository.save(client)
+    }
+
+    @Override
+    def delete(long id) throws ResourceNotFoundException {
+        log.debug("delete, id: ${id}.")
+
+        Client registrationFromDB = findById(id)
+        clientRepository.delete(registrationFromDB)
+    }
+
+}
