31b92b2d04fb30c2cadefb53660659e02038ab78
Gleb Remniov <remniov.work@gmail.com>
2019-04-14 17:44:37 +0300

    Create Client model

--- src/main/java/com/odinrossy/banksystem/models/client/Client.groovy
+++ src/main/java/com/odinrossy/banksystem/models/client/Client.groovy
@@ -1,12 +1,23 @@
 package com.odinrossy.banksystem.models.client
 
 import com.odinrossy.banksystem.models.passport.Passport
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import org.springframework.lang.NonNull
 
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.OneToOne
+import javax.persistence.Table
+import javax.validation.constraints.NotNull
 
+@Entity
+@Table(name = 'clients')
 class Client {
 
     @Id
@@ -14,8 +25,34 @@ class Client {
     @NonNull
     long id
 
+    @NotNull
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "passport_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     Passport passport
 
+    @NonNull
+    boolean isDisabled
 
+    @NonNull
+    boolean isRetiree
 
+    @NonNull
+    boolean isBoundToMilitaryService
+
+//    todo replace with BigDecimal
+    @NonNull
+    double incomePerMonth
+
+    @Override
+    String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", passport=" + passport +
+                ", isDisabled=" + isDisabled +
+                ", isRetiree=" + isRetiree +
+                ", isBoundToMilitaryService=" + isBoundToMilitaryService +
+                ", incomePerMonth=" + incomePerMonth +
+                '}'
+    }
 }
