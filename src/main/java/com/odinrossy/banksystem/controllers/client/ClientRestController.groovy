93d72b8ff4bbe844086e7cdeb766923069ce4760
Gleb Remniov <remniov.work@gmail.com>
2019-04-14 17:45:13 +0300

    Create Client RestController

---
+++ src/main/java/com/odinrossy/banksystem/controllers/client/ClientRestController.groovy
@@ -0,0 +1,86 @@
+package com.odinrossy.banksystem.controllers.client
+
+import com.odinrossy.banksystem.exceptions.ResourceNotFoundException
+import com.odinrossy.banksystem.models.client.Client
+import com.odinrossy.banksystem.services.client.ClientService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+
+@RestController
+@RequestMapping(value = '/api/client')
+class ClientRestController {
+
+    @Autowired
+    ClientService clientService
+
+    @GetMapping
+    List<Client> getAllRegistrations() {
+        try {
+            return clientService.findAll()
+
+        } catch (RuntimeException e) {
+            e.printStackTrace()
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage())
+        }
+    }
+
+    @GetMapping(value = '/{id}')
+    Client getRegistration(@PathVariable long id) {
+        try {
+            return clientService.findById(id)
+
+        } catch (ResourceNotFoundException e) {
+            e.printStackTrace()
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage())
+
+        } catch (RuntimeException e) {
+            e.printStackTrace()
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage())
+        }
+    }
+
+    @PostMapping
+    Client createRegistration(@RequestBody Client registration) {
+        try {
+            return clientService.save(registration)
+
+        } catch (RuntimeException e) {
+            e.printStackTrace()
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage())
+        }
+    }
+
+    @PutMapping(value = '/{id}')
+    Client updateRegistration(@PathVariable long id, @RequestBody Client registration) {
+        try {
+            return clientService.update(id, registration)
+
+        } catch (RuntimeException e) {
+            e.printStackTrace()
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage())
+        }
+    }
+
+    @DeleteMapping(value = '/{id}')
+    ResponseEntity deleteRegistration(@PathVariable long id) {
+        try {
+            clientService.delete(id)
+            return ResponseEntity.ok().build()
+
+        } catch (RuntimeException e) {
+            e.printStackTrace()
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage())
+        }
+    }
+
+}
