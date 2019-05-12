82528c40e350475e9d4d30588ba6b5ca3f74a4c1
Gleb Remniov <remniov.work@gmail.com>
2019-05-12 20:00:45 +0300

    add new views

--- src/main/java/com/odinrossy/banksystem/controllers/account/OperationController.groovy
+++ src/main/java/com/odinrossy/banksystem/controllers/account/OperationController.groovy
@@ -1,5 +1,6 @@
 package com.odinrossy.banksystem.controllers.account
 
+import com.odinrossy.banksystem.config.BankConfig
 import com.odinrossy.banksystem.exceptions.worker.WorkerNotAuthorizedException
 import com.odinrossy.banksystem.services.security.AuthorizationService
 import com.odinrossy.banksystem.services.worker.WorkerService
@@ -18,6 +19,18 @@ class OperationController {
     @Autowired
     AuthorizationService authorizationService
 
+    @Autowired
+    CurrencyService currencyService
+
+    @Autowired
+    ClientService clientService
+
+    @Autowired
+    ContractService contractService
+
+    @Autowired
+    AccountService accountService
+
     @RequestMapping
     def index(Model model) {
         try {
@@ -30,6 +43,140 @@ class OperationController {
             return 'redirect:/worker/logIn'
         }
         return 'operation/index'
+    }
+
+    @RequestMapping(value = '/credit')
+    def credit(Model model) {
+        try {
+            workerService.checkAuthorization()
+
+            def contracts = contractService.findAll()
+            def activeAccounts = []
+            for (contract in contracts) {
+
+                if (contract.current_account.accountType.id == BankConfig.ACCOUNT_TYPE_ACTIVE_ID ||
+                        contract.percent_account.accountType.id == BankConfig.ACCOUNT_TYPE_ACTIVE_ID) {
+
+                    def currentAccount = [:]
+                    currentAccount.id = contract.current_account.id
+                    currentAccount.name = contract.current_account.name
+                    currentAccount.currency = contract.current_account.currency
+                    currentAccount.accountType = contract.current_account.accountType
+                    currentAccount.number = contract.current_account.number
+                    currentAccount.type = contract.current_account.accountType.id == BankConfig.ACCOUNT_TYPE_ACTIVE_ID ?
+                            'Активный' : 'Пассивный'
+                    currentAccount.dateOfIssue = contract.dateOfIssue
+                    currentAccount.value = contract.current_account.value
+                    currentAccount.percentage = contract.percentage
+                    def worker = [:]
+                    worker.id = contract.worker.id
+                    worker.name = workerService.getInitials(contract.worker)
+                    currentAccount.worker = worker
+                    def client = [:]
+                    client.id = contract.client.id
+                    client.name = clientService.getInitials(contract.client)
+                    currentAccount.client = client
+                    currentAccount.contract = contract
+
+                    def percentAccount = [:]
+                    percentAccount.id = contract.percent_account.id
+                    percentAccount.name = contract.percent_account.name
+                    percentAccount.currency = contract.percent_account.currency
+                    percentAccount.accountType = contract.percent_account.accountType
+                    percentAccount.number = contract.percent_account.number
+                    percentAccount.type = contract.percent_account.accountType.id == BankConfig.ACCOUNT_TYPE_ACTIVE_ID ?
+                            'Активный' : 'Пассивный'
+                    percentAccount.dateOfIssue = contract.dateOfIssue
+                    percentAccount.value = contract.percent_account.value
+                    percentAccount.percentage = contract.percentage
+                    percentAccount.worker = worker
+                    percentAccount.client = client
+                    percentAccount.contract = contract
+
+                    activeAccounts.add(currentAccount)
+                    activeAccounts.add(percentAccount)
+                }
+            }
+
+
+            def currencies = currencyService.findAll()
+
+
+            def worker = authorizationService.workerFromSession
+            model.addAttribute('worker', worker)
+            model.addAttribute('currencies', currencies)
+            model.addAttribute('activeAccounts', activeAccounts)
+        } catch (WorkerNotAuthorizedException e) {
+            e.printStackTrace()
+            return 'redirect:/worker/logIn'
+        }
+        return 'operation/credit'
+    }
+
+   @RequestMapping(value = '/deposit')
+    def deposit(Model model) {
+        try {
+            workerService.checkAuthorization()
+
+            def contracts = contractService.findAll()
+            def passiveAccounts = []
+            for (contract in contracts) {
+
+                if (contract.current_account.accountType.id == BankConfig.ACCOUNT_TYPE_PASSIVE_ID ||
+                        contract.percent_account.accountType.id == BankConfig.ACCOUNT_TYPE_PASSIVE_ID) {
+
+                    def currentAccount = [:]
+                    currentAccount.id = contract.current_account.id
+                    currentAccount.name = contract.current_account.name
+                    currentAccount.currency = contract.current_account.currency
+                    currentAccount.accountType = contract.current_account.accountType
+                    currentAccount.number = contract.current_account.number
+                    currentAccount.type = contract.current_account.accountType.id == BankConfig.ACCOUNT_TYPE_ACTIVE_ID ?
+                            'Активный' : 'Пассивный'
+                    currentAccount.dateOfIssue = contract.dateOfIssue
+                    currentAccount.value = contract.current_account.value
+                    currentAccount.percentage = contract.percentage
+                    def worker = [:]
+                    worker.id = contract.worker.id
+                    worker.name = workerService.getInitials(contract.worker)
+                    currentAccount.worker = worker
+                    def client = [:]
+                    client.id = contract.client.id
+                    client.name = clientService.getInitials(contract.client)
+                    currentAccount.client = client
+                    currentAccount.contract = contract
+
+                    def percentAccount = [:]
+                    percentAccount.id = contract.percent_account.id
+                    percentAccount.name = contract.percent_account.name
+                    percentAccount.currency = contract.percent_account.currency
+                    percentAccount.accountType = contract.percent_account.accountType
+                    percentAccount.number = contract.percent_account.number
+                    percentAccount.type = contract.percent_account.accountType.id == BankConfig.ACCOUNT_TYPE_ACTIVE_ID ?
+                            'Активный' : 'Пассивный'
+                    percentAccount.dateOfIssue = contract.dateOfIssue
+                    percentAccount.value = contract.percent_account.value
+                    percentAccount.percentage = contract.percentage
+                    percentAccount.worker = worker
+                    percentAccount.client = client
+                    percentAccount.contract = contract
+
+                    passiveAccounts.add(currentAccount)
+                    passiveAccounts.add(percentAccount)
+                }
+            }
+
+            def currencies = currencyService.findAll()
+
+            def worker = authorizationService.workerFromSession
+            model.addAttribute('worker', worker)
+            model.addAttribute('currencies', currencies)
+            model.addAttribute('passiveAccounts', passiveAccounts)
+        } catch (WorkerNotAuthorizedException e) {
+            e.printStackTrace()
+            return 'redirect:/worker/logIn'
+        }
+        return 'operation/deposit'
     }
 
 
--- src/main/resources/static/js/operation/operationController.js
+++ src/main/resources/static/js/operation/operationController.js
@@ -6,3 +6,11 @@ function showDepositTab() {
     main.showTab('#btn-deposit-tab', ['#deposit-tab']);
 }
 
+function showCreateCreditTab() {
+    main.showTab('#btn-create-credit-tab', ['#create-credit-tab']);
+}
+
+function showAllCreditTab() {
+    main.showTab('#btn-all-credit-tab', ['#all-credit-tab']);
+}
+
